32e8f47ce0c5838e90f8968d4a02b0cd91d5b7d1
1894082 <ymodi3859@gmail.com>
2019-06-12 10:19:48 -0400

    Added LinearLayout_Main2activity

---
+++ app/src/main/java/com/example/a1894082/linearlayout/LinearLayout_Main2Activity.java
@@ -0,0 +1,13 @@
+package com.example.a1894082.linearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearLayout_Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout__main2);
+    }
+}
